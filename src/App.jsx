5337f6bbd21806632f7fab7622af7411db2d72f9
Fiqri Ramadhan Jupri <fiqrirj13@gmail.com>
2023-08-27 06:57:54 +0700

    add chose

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { BsFillPersonFill, BsRobot } from "react-icons/bs";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex mt-10">
+        <div className="m-auto">
+          <button
+            className="grid content-center mb-10 btn bg-neutral w-96 h-96 text-primary"
+            onClick={() => window.my_modal_4.showModal()}
+          >
+            <div className="flex">
+              <div className="text-center text-9xl ">
+                <BsFillPersonFill />
+              </div>
+              <h3 className="mt-5 text-5xl text-center ms-5 ">VS</h3>
+              <div className="text-center ms-10 text-9xl ">
+                <BsRobot />
+              </div>
+            </div>
+          </button>
+          <dialog id="my_modal_4" className="modal">
+            <form method="dialog" className="w-11/12 max-w-5xl modal-box">
+              <button className="absolute btn btn-sm btn-circle btn-ghost right-2 top-2">
+                ✕
+              </button>
+              <h3 className="text-lg font-bold">Hello!</h3>
+              <p className="py-4">Click the button below to close</p>
+            </form>
+          </dialog>
+          <h1 className="my-10 text-5xl font-bold text-center text-primary">
+            OR
+          </h1>
+          <button
+            className="grid content-center mb-10 btn bg-neutral w-96 h-96 text-primary"
+            onClick={() => window.my_modal_3.showModal()}
+          >
+            <div className="flex">
+              <div className="text-center text-9xl ">
+                <BsFillPersonFill />
+              </div>
+              <h3 className="mt-5 text-5xl text-center ms-5 ">VS</h3>
+              <div className="text-center ms-10 text-9xl ">
+                <BsFillPersonFill />
+              </div>
+            </div>
+          </button>
+          <dialog id="my_modal_3" className="modal">
+            <form method="dialog" className="modal-box">
+              <button className="absolute btn btn-sm btn-circle btn-ghost right-2 top-2">
+                ✕
+              </button>
+              <h3 className="text-lg font-bold">Hello!</h3>
+              <p className="py-4">
+                Press ESC key or click on ✕ button to close
+              </p>
+            </form>
+          </dialog>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
